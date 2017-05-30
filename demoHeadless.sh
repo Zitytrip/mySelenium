83f55b0b84dea16b15479d9a1f50334c569b4113
awb99 <awb99>
2017-05-30 13:30:00 -0400

    scraper root check added

--- demoHeadless.sh
+++ demoHeadless.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if [ "$(id -u)" == "0" ]; then
+   echo "This script may NOT run as Root (chromium does not work as root)"
+   exit 1
+fi
+
 sudo Xvfb :5 -once -screen 8 1024x768x8  &
 export DISPLAY=:5.8
 ./demo.sh
